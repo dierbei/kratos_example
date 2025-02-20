1b687bde2874efb8e76cfc2671161fceb8b712a8
dierbei <1628652790@qq.com>
2025-02-20 08:36:15 +0800

    feat: 实现 user entgo crud
    Signed-off-by: dierbei <1628652790@qq.com>

--- internal/data/user.go
+++ internal/data/user.go
@@ -2,7 +2,9 @@ package data
 
 import (
 	"context"
+	"fmt"
 	"hello/app/user/ent"
+	"hello/app/user/ent/user"
 
 	"hello/internal/biz"
 
@@ -22,22 +24,93 @@ func NewuserterRepo(data *Data, logger log.Logger) biz.UserRepo {
 	}
 }
 
-func (r *userterRepo) Save(context.Context, *ent.User) (*ent.User, error) {
+func (r *userterRepo) Save(ctx context.Context, req *ent.User) (*ent.User, error) {
+	client, err := ent.Open("sqlite3", "file:ent?mode=memory&cache=shared&_fk=1")
+	if err != nil {
+		log.Fatalf("failed opening connection to sqlite: %v", err)
+	}
+	defer client.Close()
+
+	//// Run the auto migration tool.
+	//if err := client.Schema.Create(context.Background()); err != nil {
+	//	log.Fatalf("failed creating schema resources: %v", err)
+	//}
+
+	u, err := client.User.Create().SetAge(req.Age).SetName(req.Name).Save(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed creating user: %v", err)
+	}
+	fmt.Println("user was created: ", u)
+
 	return nil, nil
 }
 
-func (r *userterRepo) Update(context.Context, *ent.User) (*ent.User, error) {
-	return nil, nil
+func (r *userterRepo) Update(ctx context.Context, req *ent.User) (*ent.User, error) {
+	client, err := ent.Open("sqlite3", "file:ent?mode=memory&cache=shared&_fk=1")
+	if err != nil {
+		log.Fatalf("failed opening connection to sqlite: %v", err)
+	}
+	defer client.Close()
+
+	user, err := client.User.
+		UpdateOneID(req.ID).
+		SetName(req.Name).
+		SetAge(req.Age).Save(ctx)
+
+	return user, nil
 }
 
-func (r *userterRepo) FindByID(context.Context, int64) (*ent.User, error) {
-	return nil, nil
+func (r *userterRepo) FindByID(ctx context.Context, id int64) (*ent.User, error) {
+	client, err := ent.Open("sqlite3", "file:ent?mode=memory&cache=shared&_fk=1")
+	if err != nil {
+		log.Fatalf("failed opening connection to sqlite: %v", err)
+	}
+	defer client.Close()
+
+	user, err := client.User.Get(ctx, int(id))
+	if err != nil {
+		return nil, fmt.Errorf("failed FindByID user: %v", err)
+	}
+
+	return user, nil
 }
 
-func (r *userterRepo) ListByHello(context.Context, string) ([]*ent.User, error) {
-	return nil, nil
+func (r *userterRepo) ListByHello(ctx context.Context, name string) ([]*ent.User, error) {
+	client, err := ent.Open("sqlite3", "file:ent?mode=memory&cache=shared&_fk=1")
+	if err != nil {
+		log.Fatalf("failed opening connection to sqlite: %v", err)
+	}
+	defer client.Close()
+
+	users, err := client.User.
+		Query().
+		Where(
+			user.NameContains(name),
+		).
+		Limit(10).
+		All(ctx)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed ListByHello user: %v", err)
+	}
+
+	return users, nil
 }
 
-func (r *userterRepo) ListAll(context.Context) ([]*ent.User, error) {
-	return nil, nil
+func (r *userterRepo) ListAll(ctx context.Context) ([]*ent.User, error) {
+	client, err := ent.Open("sqlite3", "file:ent?mode=memory&cache=shared&_fk=1")
+	if err != nil {
+		log.Fatalf("failed opening connection to sqlite: %v", err)
+	}
+	defer client.Close()
+
+	users, err := client.User.
+		Query().
+		All(ctx)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed ListByHello user: %v", err)
+	}
+
+	return users, nil
 }
